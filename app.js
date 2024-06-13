615f5b6ade6e82a83761c9b3b983487657034888
Porter Young <porteryoung2005@gmail.com>
2024-06-13 15:20:46 -0600

    draw heros and heal potions work now  and probably more stuff

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 let bossesDefeated = 0
 let currentGold = 0
-
+let healPots = 0
 const heroes = [
     {
         name: 'Porter',
         type: 'giant',
         damage: 5,
-        health: 100
+        health: 100,
+        maxHealth: 100,
+        level: 1
     },
     {
         name: 'Seth',
         type: 'giant',
         damage: 10,
-        health: 50
+        health: 100,
+        maxHealth: 100,
+        level: 1
     }
 ]
 
@@ -50,6 +54,7 @@ function bossAttack() {
             hero.health = 0
         }
         console.log(`${hero.name} health: `, hero.health)
+        drawHeros()
     });
 
 }
@@ -70,7 +75,50 @@ function killBoss() {
 }
 
 function levelUp(hero) {
-
+    hero.level++
+    console.log(hero.maxHealth / 4)
+    hero.maxHealth *= hero.level
+    console.log(hero.maxHealth / 4)
+    hero.health = hero.maxHealth
+    hero.damage *= Math.ceil(hero.level / 4)
+    drawHeros()
 }
 
+function buyHealPot() {
+    if (currentGold < 20) {
+        alert('ur broke')
+        return
+    }
+    healPots++
+}
+
+function healPot(heroName) {
+    let hero = heroes.find((hero) => hero.name == heroName)
+    if (healPots == 0) {
+        alert('you aint got no health potions bro')
+        return
+    }
+    let healAmount = hero.maxHealth / 4
+    hero.health += healAmount
+    if (hero.health > hero.maxHealth) {
+        hero.health = hero.maxHealth
+    }
+}
+
+function drawHeros() {
+    heroes.forEach((hero) => {
+        let heroElemenent = document.getElementById(hero.name)
+        const heroHealth = heroElemenent.querySelector('.hp')
+        const heroLevel = heroElemenent.querySelector('.lvl')
+        const heroName = heroElemenent.querySelector('.name')
+        const heroImage = heroElemenent.querySelector('.image')
+        heroName.innerHTML = `Name: ${hero.name}`
+        heroHealth.innerHTML = `HP: ${hero.health}`
+        heroLevel.innerHTML = `LVL: ${hero.level}`
+        heroImage.innerHTML = `<img src="${hero.image}" alt="">`
+    });
+}
+
+
 setInterval(bossAttack, 5000)
+drawHeros()
