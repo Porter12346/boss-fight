9085ca9d0533173fd7ec5bdd22f4e7b254c7d956
Porter Young <porteryoung2005@gmail.com>
2024-06-13 14:17:08 -0600

    html layout

---
+++ app.js
@@ -0,0 +1,21 @@
+const heroes = [
+    {
+        name: 'Porter',
+        type: 'giant',
+        damage: 5,
+        health: 100
+    },
+    {
+        name: 'Seth',
+        type: 'giant',
+        damage: 10,
+        health: 50
+    }
+]
+
+const boss = {
+    health: 100,
+    maxHealth: 100,
+    damage: 5,
+    level: 1
+}
