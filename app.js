4cd17c2fcdaf713ab53a12b105871578ab9d7e2f
Porter Young <porteryoung2005@gmail.com>
2024-06-13 14:40:29 -0600

    hittiing,killing, leveling, and more

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+let bossesDefeated = 0
+let currentGold = 0
+
 const heroes = [
     {
         name: 'Porter',
@@ -19,3 +22,55 @@ const boss = {
     damage: 5,
     level: 1
 }
+
+function hitBoss() {
+    let damage = 0
+    heroes.forEach((hero) => {
+        if (hero.health > 0) {
+            damage += hero.damage
+        }
+
+
+
+    });
+    console.log(damage)
+    boss.health -= damage
+    if (boss.health <= 0) {
+        boss.health = 0
+        killBoss()
+    }
+    console.log(boss.health)
+
+}
+
+function bossAttack() {
+    heroes.forEach((hero) => {
+        hero.health -= boss.damage
+        if (hero.health < 0) {
+            hero.health = 0
+        }
+        console.log(`${hero.name} health: `, hero.health)
+    });
+
+}
+
+function killBoss() {
+
+    currentGold += (boss.level * 10)
+
+    heroes.forEach((hero) => {
+        levelUp(hero)
+    });
+
+    boss.level++
+    boss.maxHealth *= boss.level
+    boss.health = boss.maxHealth
+    boss.damage *= boss.level
+    bossesDefeated++
+}
+
+function levelUp(hero) {
+
+}
+
+setInterval(bossAttack, 5000)
